c39168679746fd986c57d4e9a1d9e2d58d9ec156
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-19 20:46:15 +0300

    chore: add classes to the horizontal line

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -90,7 +90,7 @@ const ResumeBuilder = () => {
           <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
             <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
               {/* Progress bar using activeSectionIndex */}
-              <hr />
+              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
             </div>
           </div>
           {/* Right Panel - Form */}
