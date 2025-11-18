ffca9bb5168024e9c6029d727661b2f5b50919b9
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-18 16:36:46 +0300

    chore: add a theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Button } from "./components/ui/button";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
+import { ThemeProvider } from "./context/theme-provider";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Layout>Hello</Layout>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>Hello</Layout>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
